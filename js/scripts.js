da5af8789962028b63067be5515c8eaf3e51e5d6
Noah Ramey <noahramey1997@gmail.com>
2016-06-16 11:39:59 -0700

    rescope 'numberArray' variable into specific functions instead of having a global variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,39 @@
 // BACK END //
-var numberArray = [];
 
-// spec 1: count up to the specified number. //
 var countUp = function(number) {
+  var numberArray = [];
   for (var i = 1; i <= number; i++) {
     numberArray.push(i);
   }
+  return numberArray;
 }
 
-// spec 2: numbers divisible by 3 are replaced with "ping". //
-var ping = function(number) {
-  numberArray.forEach(function(number) {
-    if (number % 3 === 0 && number % 5 !== 0) {
-      numberArray[number-1] = "ping";
-    }
-  });
-}
-
-// spec 3: numbers divisible by 5 are replaced with "pong". //
-var pong = function(number) {
-  numberArray.forEach(function(number) {
-    if (number % 5 === 0 && number % 3 !== 0) {
-      numberArray[number-1] = "pong";
-    }
-  });
-}
-
-
-// spec 4: numbers divisible by 15 are replaced with "pingpong". //
 var pingPong = function(number) {
+  var numberArray = countUp(number);
   numberArray.forEach(function(number) {
     if (number % 15 === 0) {
       numberArray[number-1] = "pingpong";
+    } else if (number % 5 === 0) {
+      numberArray[number-1] = "pong";
+    } else if (number % 3 === 0) {
+      numberArray[number-1] = "ping";
     }
   });
+  return numberArray;
 }
-
-var replaceDivisible = function(number) {
-  ping(number);
-  pong(number);
-  pingPong(number);
-}
-
-
 
 // FRONT END //
 $(function() {
   $("#input").submit(function(event) {
-
+    event.preventDefault();
     $("#output").empty();
 
     var input = parseInt($("#number").val());
-    countUp(input);
-    replaceDivisible(numberArray);
+
+    var numberArray = pingPong(input);
 
     numberArray.forEach(function(number) {
-    $("#output").append("<li>" + number + "</li>");
+      $("#output").append("<li>" + number + "</li>");
     });
-
-    numberArray = [];
-
-    event.preventDefault();
   });
 });
